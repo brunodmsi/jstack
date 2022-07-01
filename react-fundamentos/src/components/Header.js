a9940eb322a6312eb61dff3105d0caf48751ca35
Bruno De Masi <brunodemasi1@gmail.com>
2022-06-30 21:05:37 -0300

    feat: css modules

--- react-fundamentos/src/components/Header.js
+++ react-fundamentos/src/components/Header.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import Button from "./Button";
 import { ThemeContext } from "../context/ThemeContext";
 
+import styles from './Header.css';
+
 const Header = ({ children, title = "JStack's Blog" }) => {
 	const { onToggleTheme } = useContext(ThemeContext);
 
 	return (
 		<>
-			<h1>{title}</h1>
+			<h1 className={styles.title}>{title}</h1>
 
 			<Button onClick={onToggleTheme}>
 				Mudar tema
