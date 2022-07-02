8d5797d85c36aee698babe0786279fae65766cc2
Bruno De Masi <brunodemasi1@gmail.com>
2022-07-01 21:53:36 -0300

    feat: revamp dos looks

---
+++ react-fundamentos/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Footer = () => {
+	return (
+		<Container>
+			<span>JStack's blog. Todos os direitos reservados</span>
+			<button type="button">🌞</button>
+		</Container>
+	);
+};
+
+export default Footer;
--- react-fundamentos/src/components/Header/index.js
+++ react-fundamentos/src/components/Header/index.js
@@ -1,31 +1,14 @@
-import React, { useContext } from "react";
-import PropTypes from 'prop-types';
-import styled from "styled-components";
+import React from 'react';
 
-import Button from "../Button";
-import Title from "../Title";
+import { Container } from './styles';
 
-import { ThemeContext } from "../../context/ThemeContext";
-
-const Header = ({ children, title = "JStack's Blog" }) => {
-	const { onToggleTheme } = useContext(ThemeContext);
-
-	return (
-		<>
-			<Title>{title}</Title>
-
-			<Button onClick={onToggleTheme}>
-				Mudar tema
-			</Button>
-		
-			{children}
-		</>
-	);
-}
-
-Header.propTypes = {
-	title: PropTypes.string,
-	children: PropTypes.node,
+const Header = () => {
+  return (
+    <Container>
+      <h1>JStack's Blog</h1>
+      <button type="button">🌞</button>
+    </Container>
+  );
 }
 
 export default Header;
