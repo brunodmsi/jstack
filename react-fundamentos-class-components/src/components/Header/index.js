0ad859263565ea661462bf35c48a236f18719237
Bruno De Masi <brunodemasi1@gmail.com>
2022-07-06 23:21:34 -0300

    inicio de class..

--- react-fundamentos-class-components/src/components/Header/index.js
+++ react-fundamentos-class-components/src/components/Header/index.js
@@ -3,20 +3,25 @@ import React from 'react';
 import { useTheme } from '../../hooks/theme';
 import { Container } from './styles';
 
-const Header = () => {
-  const { selectedTheme, onToggleTheme } = useTheme();
-
-  return (
-    <Container>
-      <h1>JStack's Blog</h1>
-			<button 
-				type="button" 
-				onClick={onToggleTheme}
-			>
-				{selectedTheme === 'dark' ? '🌞' : '🌚'}
-			</button>
-    </Container>
-  );
+class Header extends React.Component {
+	render() {
+		return (
+			<Container>
+				<h1>JStack's Blog</h1>
+				<button 
+					type="button" 
+					onClick={onToggleTheme}
+				>
+					{selectedTheme === 'dark' ? '🌞' : '🌚'}
+				</button>
+			</Container>
+		);
+	}
 }
+
+//   const { selectedTheme, onToggleTheme } = useTheme();
+//   return (
+//   );
+// }
 
 export default Header;
--- react-fundamentos-class-components/src/components/PostsList/index.js
+++ react-fundamentos-class-components/src/components/PostsList/index.js
@@ -5,18 +5,20 @@ import Post from './Post';
 
 import posts from './posts';
 
-const PostsList = () => {
-	return (
-		<Container>
-			{posts.map((post) => (
-				<Post 
-					key={post.id}	
-					title={post.title}
-					description={post.description}
-				/>
-			))}
-		</Container>
-	)
-};
+class PostsList extends React.Component {
+	render() {
+		return (
+			<Container>
+				{posts.map((post) => (
+					<Post 
+						key={post.id}	
+						title={post.title}
+						description={post.description}
+					/>
+				))}
+			</Container>
+		);
+	}
+}
 
 export default PostsList
