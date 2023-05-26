7af16210fa45411588cc36142165284479b9296c
Bruno De Masi <bruno@demasi.dev>
2023-05-25 23:34:36 -0300

    feat: componentes de input, select e button

--- mycontacts/frontend/src/pages/NewContact/index.js
+++ mycontacts/frontend/src/pages/NewContact/index.js
@@ -1,5 +1,24 @@
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewContact() {
-	return <PageHeader title="Novo contato" />;
+	return (
+		<>
+			<PageHeader title="Novo contato" />
+			<Input placeholder="Nome" />
+			<Select>
+				<option value="123"> instagram</option>
+				<option value="123"> instagram</option>
+				<option value="123"> instagram</option>
+				<option value="123"> instagram</option>
+				<option value="123"> instagram</option>
+			</Select>
+			<Button type="button">Salvar alterações</Button>
+			<Button disabled type="button">
+				Salvar alterações
+			</Button>
+		</>
+	);
 }
