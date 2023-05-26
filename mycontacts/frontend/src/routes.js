f0e0bb3856c9089bbba52aa6b0f10bbcfb8ab222
Bruno De Masi <bruno@demasi.dev>
2023-05-25 23:17:51 -0300

    feat: page header com title prop

--- mycontacts/frontend/src/routes.js
+++ mycontacts/frontend/src/routes.js
@@ -8,7 +8,7 @@ export default function Routes() {
 	return (
 		<Switch>
 			<Route path="/" exact component={Home} />
-			<Route path="new" component={NewContact} />
+			<Route path="/new" component={NewContact} />
 			<Route path="/edit/:id" component={EditContact} />
 		</Switch>
 	);
