c35aef4a939dc66e66cb53ad49edca99c60f33e0
Bruno De Masi <bruno@demasi.dev>
2023-05-29 23:08:38 -0300

    feat: controlled componentes no form

--- mycontacts/frontend/src/components/ContactForm/index.js
+++ mycontacts/frontend/src/components/ContactForm/index.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
@@ -6,23 +8,50 @@ import Button from '../Button';
 import { Form, ButtonContainer } from './styles';
 
 export default function ContactForm({ buttonLabel }) {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [phone, setPhone] = useState('');
+	const [category, setCategory] = useState('');
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		console.log({ name, email, phone, category });
+	}
+
 	return (
-		<Form>
-			<FormGroup error="Teste">
-				<Input placeholder="Nome" error />
+		<Form onSubmit={handleSubmit}>
+			<FormGroup>
+				<Input
+					value={name}
+					placeholder="Nome"
+					onChange={(event) => setName(event.target.value)}
+				/>
 			</FormGroup>
 
 			<FormGroup>
-				<Input placeholder="E-mail" />
+				<Input
+					value={email}
+					placeholder="E-mail"
+					onChange={(event) => setEmail(event.target.value)}
+				/>
 			</FormGroup>
 
 			<FormGroup>
-				<Input placeholder="Telefone" />
+				<Input
+					value={phone}
+					placeholder="Telefone"
+					onChange={(event) => setPhone(event.target.value)}
+				/>
 			</FormGroup>
 
 			<FormGroup>
-				<Select>
+				<Select
+					value={category}
+					onChange={(event) => setCategory(event.target.value)}
+				>
+					<option value="">Categoria</option>
 					<option value="instagram">Instagram</option>
+					<option value="discord">Discord</option>
 				</Select>
 			</FormGroup>
 
