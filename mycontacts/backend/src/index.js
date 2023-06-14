7e4d8f7b02a5d3b5596cf5555d7b6041b4422ec2
Bruno De Masi <bruno@demasi.dev>
2023-06-14 00:02:10 -0300

    feat: abstraindo middlewares

--- mycontacts/backend/src/index.js
+++ mycontacts/backend/src/index.js
@@ -2,25 +2,21 @@ const express = require('express');
 require('express-async-errors');
 
 const routes = require('./routes');
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 
-app.use((_, response, next) => {
-	response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-	next();
-});
+app.use(cors);
 
 app.use(routes);
 
 /**
  * Error Handler
  */
-app.use((error, _, response, _) => {
-	console.log(error);
-	response.sendStatus(500);
-});
+app.use(errorHandler);
 
 const port = 8000;
 app.listen(port, () => `Running on port ${port}`);
